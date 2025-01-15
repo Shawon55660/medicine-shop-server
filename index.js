274bd1c327fb2e354e03f3d62ed4c047e1359b2e
shawonahme <shawonahmed55660@gmail.com>
2025-01-15 15:21:38 +0600

    role get api and verify token api make done

--- index.js
+++ index.js
@@ -24,6 +24,8 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
 
+    //all colection here
+    const usersCollection =   client.db('mediStore').collection('users')
     //jwt related api
     app.post('/jwt',async(req,res)=>{
         const user = req.body;
@@ -31,8 +33,79 @@ async function run() {
 
         res.send({token})
     })
+    //verify token API
+    const verifyToken =  (req,res,next)=>{
+      if(!req.headers.authorization){
+        return res.status(401).send({ message: 'unauthorized ki' });
+
+      }
+
+      const token = req.headers.authorization.split(' ')[1];
+      jwt.verify(token,process.env.ACCESS_TOKEN_SECRET,(error,decoded)=>{
+        if(error){
+          return res.status(401).send({ message: 'unauthorized access' })
+        }
+        req.decoded =  decoded
+        next()
+      })
+
+    }
+
+    //users info API
+    app.post('/users',async(req,res)=>{
+      const userInfo = req.body;
+      const userEmail = userInfo.email 
+     const filter = {email:userEmail}
+     const alreadyExiting = await usersCollection.findOne(filter)
+     if(alreadyExiting) {
+      return
+     } else{
+      const result = await usersCollection.insertOne(userInfo)
+      res.send(result)
+
+     }
+
+     
+
+    })
+
+    //check email and get role
+    app.get('/users/role/:email',verifyToken, async(req,res)=>{
+      const email = req.params.email;
+      if (email !== req.decoded.email) return res.status(403).send({ message: 'forbidden access' })
+
+     
+
+        const query = {email:email};
+        const user = await usersCollection.findOne(query)
+        
+        if(user){
+          const role = user?.role;
+        
+          res.send({role})
+        }
+        
+      
+    })
+    // //check email is Seller
+    // app.get('/users/seller/:email',verifyToken, async(req,res)=>{
+    //   const email = req.params.email;
+    //   if (email !== req.decoded.email) return res.status(403).send({ message: 'forbidden access' })
+
+       
+
+    //     const query = {email:email};
+    //     const user = await usersCollection.findOne(query)
+    //     let seller = false
+    //     if(user){
+    //       seller = user?.role === 'seller';
+    //     }
+    //     res.send({seller})
+      
+    // })
    
-  } finally {
+  }
+   finally {
    
   }
 }
